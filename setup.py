b6b5ba9e77c0e2ad5956d56a949258c53ace4cec
nynra <nynradev@pm.me>
2024-02-20 13:20:30 +0100

    updated the dependancies in the setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="Pypsst",
+    name="pypsst",
     version="0.0.4",
     author="Nynra",
     description="A Python package for simple encryption tasks",
     py_modules=["pypsst"],
     package_dir={"": "src"},
-    install_requires=["pycryptodome", "rlp"],
+    install_requires=[list(map(str.strip, open("requirements.txt").readlines()))],
     entry_points="""
         [console_scripts]
         pypsst=pypsst.cli.cli:cli
